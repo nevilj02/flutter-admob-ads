d02efab90312fa7d75b4f4da934ecb3a3e7c6821
nevilj02 <nevilgorasiya.work@gmail.com>
2025-05-19 04:25:17 +0530

    Update ga_service.dart

--- lib/ga_service.dart
+++ lib/ga_service.dart
@@ -3,8 +3,8 @@ import 'package:ga_native_ad/ga_native_ad.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class GAService {
-  void init({required String videoNativeAdId}) {
-    MobileAds.instance.initialize();
+  Future<void> init({required String videoNativeAdId}) async {
+    await MobileAds.instance.initialize();
     GANativeAd.init(videoNativeAdId: videoNativeAdId);
   }
 
